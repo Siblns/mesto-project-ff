cde65328ae30f5ec3e0d240dbb4116f9a156572d
lebedevans <lebedevanat95@yandex.ru>
2024-12-18 03:14:29 +0700

    first version work

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,39 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
+const cardMain = initialCards.map(function (item) {
+  return {
+    name: item.name,
+    link: item.link
+  };
+});
+
+// @todo: функция вывода на страницу
+function renderCards() {
+  cardMain.forEach(renderCard);
+}
 
 // @todo: Функция создания карточки
+function renderCard({name, link}) {
+  const placeElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  const cardDeleteButton = placeElement.querySelector('.card__delete-button');
+
+  placeElement.querySelector('.card__title').textContent = name;
+  placeElement.querySelector('.card__image').src = link;
+
+  cardDeleteButton.addEventListener('click', function () {
+    cardDelete(this.parentElement);
+  });
+
+  placesList.prepend(placeElement);
+};
 
 // @todo: Функция удаления карточки
+function cardDelete(el) {
+  el.remove();
+};
 
 // @todo: Вывести карточки на страницу
+renderCards();
