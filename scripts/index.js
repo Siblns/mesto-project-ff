023692618b01044e5d72714334ac5d2f6b4f5d0c
lebedevans <lebedevanat95@yandex.ru>
2024-12-19 23:22:49 +0700

    fix pjoject after review

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,29 @@
-// @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
-
-// @todo: DOM узлы
 const placesList = document.querySelector('.places__list');
-const cardMain = initialCards.map(function (item) {
-  return {
-    name: item.name,
-    link: item.link
-  };
-});
 
-// @todo: функция вывода на страницу
-function renderCards() {
-  cardMain.forEach(renderCard);
-}
-
-// @todo: Функция создания карточки
-function renderCard({name, link}) {
-  const placeElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+function createCard({name, link}, deleteElement) {
+  const placeElement = cardTemplate.cloneNode(true);
   const cardDeleteButton = placeElement.querySelector('.card__delete-button');
+  const cardTitle = placeElement.querySelector('.card__title');
+  const cardImage = placeElement.querySelector('.card__image');
 
-  placeElement.querySelector('.card__title').textContent = name;
-  placeElement.querySelector('.card__image').src = link;
-
+  cardTitle.textContent = name;
+  cardImage.src = link;
+  cardImage.setAttribute("alt", name);
+  
   cardDeleteButton.addEventListener('click', function () {
-    cardDelete(this.parentElement);
+    deleteElement(this.parentElement);
   });
 
-  placesList.prepend(placeElement);
-};
+  return placeElement;
+}
 
-// @todo: Функция удаления карточки
-function cardDelete(el) {
+function removeCard(el) {
   el.remove();
 };
 
-// @todo: Вывести карточки на страницу
-renderCards();
+function renderCards(item, method = "prepend") {
+  item.forEach(placeItem => placesList[ method ](createCard(placeItem, removeCard)));
+};
+
+renderCards(initialCards, 'prepend');
